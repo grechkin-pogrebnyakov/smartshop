88f62fc6177ad5fd86be120c4c9201b4a7ef3391
Serg Titaevskiy <Titaevskiy.S@gmail.com>
2015-11-07 18:28:01 +0300

    Add chart to main activity

--- app/src/main/java/com/technopark/smartbiz/MainActivity.java
+++ app/src/main/java/com/technopark/smartbiz/MainActivity.java
@@ -4,18 +4,15 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
-
+import com.github.mikephil.charting.charts.LineChart;
 import com.google.zxing.integration.android.IntentIntegrator;
 import com.google.zxing.integration.android.IntentResult;
-import com.technopark.smartbiz.buisnessLogic.addProduct.AddProductActivity;
 import com.technopark.smartbiz.screnListView.ListAddedProducts;
 import com.technopark.smartbiz.userIdentification.LoginActivity;
 
@@ -34,14 +31,8 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        LineChart mainChart = (LineChart) findViewById(R.id.activity_main_chart);
+
 
         Button logOut = (Button) findViewById(R.id.button_logout);
 
