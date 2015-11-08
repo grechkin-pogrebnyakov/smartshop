46a341122df59d66b640c40819d9efcc9bc357ee
Serg Titaevskiy <Titaevskiy.S@gmail.com>
2015-11-08 19:51:08 +0300

    Add payment activity

--- app/src/main/java/com/technopark/smartbiz/CheckActivity.java
+++ app/src/main/java/com/technopark/smartbiz/CheckActivity.java
@@ -1,13 +1,29 @@
 package com.technopark.smartbiz;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class CheckActivity extends AppCompatActivity {
+
+	private float totalPrice = 123.45f;
+	public static String TOTAL_PRICE = "TotalPrice";
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_check);
+
+		Button button = (Button) findViewById(R.id.activity_check_button_submit);
+		button.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				Intent submit = new Intent(getApplicationContext(), PaymentActivity.class);
+				submit.putExtra(TOTAL_PRICE, totalPrice);
+				startActivity(submit);
+			}
+		});
 	}
 }
---
+++ app/src/main/java/com/technopark/smartbiz/PaymentActivity.java
@@ -0,0 +1,53 @@
+package com.technopark.smartbiz;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.util.Locale;
+
+public class PaymentActivity extends AppCompatActivity implements View.OnFocusChangeListener {
+
+	private float totalPrice = 0.0f;
+	private float oddMoney = 0.0f;
+	private float payment = 0.0f;
+
+	private EditText paymentEditText;
+	private TextView oddMoneyTextView;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_payment);
+
+		Bundle extras = getIntent().getExtras();
+		if (extras != null) {
+			totalPrice = extras.getFloat(CheckActivity.TOTAL_PRICE);
+		}
+
+		TextView totalTextView = (TextView) findViewById(R.id.activity_payment_textview_total);
+		totalTextView.setText(String.format(Locale.US, "%.2f", totalPrice));
+
+		oddMoneyTextView = (TextView) findViewById(R.id.activity_payment_textview_odd_money);
+		updateOddMoneyEditText();
+
+		paymentEditText = (EditText) findViewById(R.id.activity_payment_edittext_payment);
+		paymentEditText.setOnFocusChangeListener(this);
+	}
+
+	private void updateOddMoneyEditText() {
+		oddMoney = payment - totalPrice;
+		oddMoneyTextView.setText(String.format(Locale.US, "%.2f", oddMoney));
+	}
+
+	// TODO Change to key press event
+	@Override
+	public void onFocusChange(View v, boolean hasFocus) {
+		if (!hasFocus) {
+			payment = Float.valueOf(paymentEditText.getText().toString());
+			updateOddMoneyEditText();
+		}
+	}
+}
