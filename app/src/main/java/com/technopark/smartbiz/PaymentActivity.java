2db4dff80ace77547e7dbcfb7ee8e42af2862909
Serg Titaevskiy <Titaevskiy.S@gmail.com>
2015-11-10 09:40:48 +0300

    Clear stack history at the transition from the Payment activity to the Main activity

--- app/src/main/java/com/technopark/smartbiz/PaymentActivity.java
+++ app/src/main/java/com/technopark/smartbiz/PaymentActivity.java
@@ -1,8 +1,10 @@
 package com.technopark.smartbiz;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
@@ -35,6 +37,16 @@ public class PaymentActivity extends AppCompatActivity implements View.OnFocusCh
 
 		paymentEditText = (EditText) findViewById(R.id.activity_payment_edittext_payment);
 		paymentEditText.setOnFocusChangeListener(this);
+
+		Button doneButton = (Button) findViewById(R.id.activity_payment_button_pay);
+		doneButton.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				Intent submit = new Intent(getApplicationContext(), MainActivity.class);
+				submit.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+				startActivity(submit);
+			}
+		});
 	}
 
 	private void updateOddMoneyEditText() {
