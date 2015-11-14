f8163969b24042988f22e3496369ec2dc3b2b354
Serg Titaevskiy <Titaevskiy.S@gmail.com>
2015-11-14 16:51:16 +0300

    Implement Parcelable in Check

--- app/src/main/java/com/technopark/smartbiz/database/items/Check.java
+++ app/src/main/java/com/technopark/smartbiz/database/items/Check.java
@@ -1,72 +1,111 @@
 package com.technopark.smartbiz.database.items;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 /**
  * Created by Abovyan on 14.11.15.
  */
-public class Check {
+public class Check implements Parcelable {
 
-    private String checkName;
-    private String photoPath;
-    private int priceSellingProduct;
-    private int pricePurchaseProduct;
-    private int idFromProductsTable;
-    private int count;
+	private String checkName;
+	private String photoPath;
+	private int priceSellingProduct;
+	private int pricePurchaseProduct;
+	private int idFromProductsTable;
+	private int count;
 
 
-    public Check(String checkName, String photoPath, int priceSellingProduct, int pricePurchaseProduct, int idFromProductsTable, int count) {
-        this.checkName = checkName;
-        this.photoPath = photoPath;
-        this.idFromProductsTable = idFromProductsTable;
-        this.count = count;
-        this.priceSellingProduct = priceSellingProduct;
-        this.pricePurchaseProduct = pricePurchaseProduct;
-    }
+	public Check(String checkName, String photoPath, int priceSellingProduct, int pricePurchaseProduct, int idFromProductsTable, int count) {
+		this.checkName = checkName;
+		this.photoPath = photoPath;
+		this.idFromProductsTable = idFromProductsTable;
+		this.count = count;
+		this.priceSellingProduct = priceSellingProduct;
+		this.pricePurchaseProduct = pricePurchaseProduct;
+	}
 
-    public String getCheckName() {
-        return checkName;
-    }
+	public Check(Parcel source) {
+		source.readString();
+		source.readString();
+		source.readInt();
+		source.readInt();
+		source.readInt();
+		source.readInt();
+	}
 
-    public void setCheckName(String checkName) {
-        this.checkName = checkName;
-    }
+	public String getCheckName() {
+		return checkName;
+	}
 
-    public String getPhotoPath() {
-        return photoPath;
-    }
+	public void setCheckName(String checkName) {
+		this.checkName = checkName;
+	}
 
-    public void setPhotoPath(String photoPath) {
-        this.photoPath = photoPath;
-    }
+	public String getPhotoPath() {
+		return photoPath;
+	}
 
-    public int getPriceSellingProduct() {
-        return priceSellingProduct;
-    }
+	public void setPhotoPath(String photoPath) {
+		this.photoPath = photoPath;
+	}
 
-    public void setPriceSellingProduct(int priceSellingProduct) {
-        this.priceSellingProduct = priceSellingProduct;
-    }
+	public int getPriceSellingProduct() {
+		return priceSellingProduct;
+	}
 
-    public int getPricePurchaseProduct() {
-        return pricePurchaseProduct;
-    }
+	public void setPriceSellingProduct(int priceSellingProduct) {
+		this.priceSellingProduct = priceSellingProduct;
+	}
 
-    public void setPricePurchaseProduct(int pricePurchaseProduct) {
-        this.pricePurchaseProduct = pricePurchaseProduct;
-    }
+	public int getPricePurchaseProduct() {
+		return pricePurchaseProduct;
+	}
 
-    public int getIdFromProductsTable() {
-        return idFromProductsTable;
-    }
+	public void setPricePurchaseProduct(int pricePurchaseProduct) {
+		this.pricePurchaseProduct = pricePurchaseProduct;
+	}
 
-    public void setIdFromProductsTable(int idFromProductsTable) {
-        this.idFromProductsTable = idFromProductsTable;
-    }
+	public int getIdFromProductsTable() {
+		return idFromProductsTable;
+	}
 
-    public int getCount() {
-        return count;
-    }
+	public void setIdFromProductsTable(int idFromProductsTable) {
+		this.idFromProductsTable = idFromProductsTable;
+	}
 
-    public void setCount(int count) {
-        this.count = count;
-    }
+	public int getCount() {
+		return count;
+	}
+
+	public void setCount(int count) {
+		this.count = count;
+	}
+
+	@Override
+	public int describeContents() {
+		return 0;
+	}
+
+	@Override
+	public void writeToParcel(Parcel dest, int flags) {
+		dest.writeString(checkName);
+		dest.writeString(photoPath);
+		dest.writeInt(priceSellingProduct);
+		dest.writeInt(pricePurchaseProduct);
+		dest.writeInt(idFromProductsTable);
+		dest.writeInt(count);
+	}
+
+	public static final Parcelable.Creator<Check> CREATOR = new Creator<Check>() {
+		@Override
+		public Check createFromParcel(Parcel source) {
+			return new Check(source);
+		}
+
+		@Override
+		public Check[] newArray(int size) {
+			return new Check[size];
+		}
+	};
 }
