e1165fb0235c35bf2998ef38bd0db14597989208
mid135 <moskalukdima@mail.ru>
2015-11-15 00:39:58 +0300

    staging commit

--- rest_auth/vkontakte.py
+++ rest_auth/vkontakte.py
@@ -1,10 +1,10 @@
 __author__ = 'mid'
 
-from allauth.socialaccount.providers.vk.provider import VKProvider
+from allauth.socialaccount.providers.vk.provider import VKProvider,OAuth2Provider
 from rest_auth.registration.views import SocialLoginView
 
 class VkLogin(SocialLoginView):
-    adapter_class = VKProvider
+    adapter_class = OAuth2Provider
 
 
 
