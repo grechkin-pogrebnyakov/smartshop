636e93d6f82115a3e24694436100d1a827dc57b6
mid135 <moskalukdima@mail.ru>
2015-11-15 01:14:55 +0300

    staging commit

--- rest_auth/vkontakte.py
+++ rest_auth/vkontakte.py
@@ -2,8 +2,11 @@ __author__ = 'mid'
 
 from allauth.socialaccount.providers.vk.provider import VKProvider,OAuth2Provider,VKAccount
 from rest_auth.registration.views import SocialLoginView
+from allauth.socialaccount.providers.oauth2.client import OAuth2Client
 
 class VkLogin(SocialLoginView):
     adapter_class = VKProvider
+    client_class = OAuth2Client
+    callback_url = 'https://smartshop1.ddns.net:81/api/auth/vk/'
 
 
