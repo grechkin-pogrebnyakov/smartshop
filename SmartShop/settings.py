93060c31fb37b0cf7d4fe54cb7608fe6c6beda85
mid135 <moskalukdima@mail.ru>
2015-11-26 18:33:19 +0300

    session disabled

--- SmartShop/settings.py
+++ SmartShop/settings.py
@@ -203,8 +203,8 @@ LOGGING = {
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.TokenAuthentication',
-        'rest_framework.authentication.BasicAuthentication',
-        'rest_framework.authentication.SessionAuthentication',
+        #'rest_framework.authentication.BasicAuthentication',
+        #'rest_framework.authentication.SessionAuthentication',
     )
 }
 
