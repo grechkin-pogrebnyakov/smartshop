1fe472bdf4178ff4107e496210c3c80b295c7730
mid135 <moskalukdima@mail.ru>
2015-11-27 23:35:35 +0300

    move employee registration to other url

--- SmartShop/urls.py
+++ SmartShop/urls.py
@@ -1,9 +1,11 @@
 from django.conf.urls import include, url,patterns
 from django.contrib import admin
+from rest_auth.registration.views import RegisterEmployeeView
 
 urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
     url(r'^api/auth/',include('rest_auth.urls')),
     url(r'^api/store/',include('storeManage.urls')),
     url(r'^api/profile/',include('userManage.urls')),
+    url(r'^api/employee/register/$',RegisterEmployeeView.as_view()),
     )
