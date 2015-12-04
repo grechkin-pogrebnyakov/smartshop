10353552695ddb9fea612fcaa9576b1e26cc4560
Sergey Grechkin-Pogrebnyakov <grechkin.pogrebnyakov@gmail.com>
2015-12-04 14:38:22 +0300

    fix db password

--- SmartShop/settings.py
+++ SmartShop/settings.py
@@ -92,7 +92,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'SmartShop',
         'USER': 'root',
-        #'PASSWORD': '1234QWer',
+        'PASSWORD': '1234QWer',
         'HOST': 'localhost',
         'PORT': '3306',
         'OPTIONS': {
